b3673f520898f486bcdc29291bd472d4fa5af894
Dommaraju Venkata <bdommarajuvenkata@deloitte.ie>
2020-03-17 19:53:51 +0000

    adding initial setup to git hub

---
+++ src/main/java/com/Java/practice/PracticeApplication.java
@@ -0,0 +1,15 @@
+package com.Java.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.Java"})
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
